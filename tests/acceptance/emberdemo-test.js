3f85396f8c2d3ead105e8253279a25d83b774288
GP <GP>
2016-12-09 13:27:15 +0530

    Demo application for ember build in circle ci with test

---
+++ tests/acceptance/emberdemo-test.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'emberdemo/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | emberdemo');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+	assert.equal(find('h2').text(), "Grand Old Mansion", 'should list rental title');
+  });
+});
